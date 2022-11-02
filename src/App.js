871a5f3f2ce787dcd5d8e6498904d8ddb2615809
Pradip Sarkar <sarkar.pradip2002@gmailcom>
2022-11-02 10:15:53 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import AllRoutes from './AllRoutes';
+import { getquestions } from './actions/askquestions';
+import { getusers } from './actions/users';
 
 function App() {
+  var dispatch=useDispatch();
+  
+  useEffect(() => {
+    dispatch(getquestions());
+    dispatch(getusers());
+  }, [dispatch])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar/>
+        <AllRoutes/>
+      </BrowserRouter>
     </div>
   );
 }
