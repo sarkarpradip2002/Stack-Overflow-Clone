3540a5df380fcf1364ae74110f29b926aa95b5e6
Pradip Sarkar <85306445+sarkarpradip2002@users.noreply.github.com>
2023-02-19 13:01:41 +0000

    Changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API=axios.create({baseURL : 'https://stack-overflow-clone-pradip.herokuapp.com'})
+const API=axios.create({baseURL : 'https://stackoverflowclone-backend.up.railway.app/'})
 
 export const signup=(authdata)=> API.post('/user/signup',authdata);
 export const login=(authdata)=> API.post('/user/login',authdata);
